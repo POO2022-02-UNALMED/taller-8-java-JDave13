05c3a4f5c84b2e7fab913942577a6a24dc3454b5
JDave13 <111092059+JDave13@users.noreply.github.com>
2022-10-17 15:32:49 -0500

    intento 1

---
+++ src/main/java/futbol/Jugador.java
@@ -0,0 +1,37 @@
+package futbol;
+
+public class Jugador extends Futbolista {
+    public short golesMarcados;
+    private  byte dorsal;
+
+    public Jugador(){
+        super();
+        this._golesMarcados = 289;
+        this.dorsal = 7;
+    }
+
+    public Jugador(String nombre, int edad, String posicion,short golesMarcados,byte dorsal){
+        super(nombre,edad,posicion);
+        this.golesMarcados = golesMarcados;
+        this.dorsal = dorsal;
+    }
+    @Override
+    public boolean jugarConLasManos() {
+        return false;
+    }
+    @Override
+    public int compareTo(Object f) {
+        return Math.abs( this.getEdad() - ((Jugador) f).getEdad() );
+    }
+
+    @Override
+    public String toString() {
+        return super.toString() + " con el dorsal " + dorsal + ". Ha marcado " + golesMarcados;
+    }
+
+    public short getGolesMarcados() {return golesMarcados;}
+    public void setGolesMarcados(short golesMarcados) {this.golesMarcados = golesMarcados;}
+
+    public byte getDorsal() {return dorsal;}
+    public void setDorsal(byte dorsal) {this.dorsal = dorsal;}
+}
